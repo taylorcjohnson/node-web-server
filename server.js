424df314d4a5ecc237b2022f3560fb2dd8ea17b3
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-08-25 15:10:02 -0400

    Prepare for heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
 let app = express()
 
 // Tells handlebars - hbs where the partials are located
@@ -63,6 +64,6 @@ app.get('/bad', (req, res) => {
 })
 
 // Bind application to port on machine
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
